95a701c084e5127d46222f06ab1187db3eedfd0b
Hikmahx <hikmayousuph@gmail.com>
2022-04-20 14:46:56 -1100

    packages installed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App relative bg-dark font-Manrope'>
+      Hi from react
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors : {  
+      "light-cyan": "#cee3e9",
+      "neon-green": "#52ffa8",
+      'grayish-blue': '#4e5d73',
+      'dark-grayish-blue': '##323a49',
+      'dark-blue': '#1f2632',
+      'transparent': 'transparent',
+      'white': '#ffffff',
+      'black': '#000000'
+    },
+    fontFamily: {
+      Manrope : ['Manrope', 'san-serif']
+    }
+  },
+  plugins: [],
+}
