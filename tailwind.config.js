91e4390f27527b9dd49e0ed096704a039927b9bc
Hikmahx <hikmayousuph@gmail.com>
2022-04-21 12:30:24 -1100

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       "light-cyan": "#cee3e9",
       "neon-green": "#52ffa8",
       'grayish-blue': '#4e5d73',
-      'dark-grayish-blue': '##323a49',
+      'dark-grayish-blue': '#323a49',
       'dark-blue': '#1f2632',
       'transparent': 'transparent',
       'white': '#ffffff',
