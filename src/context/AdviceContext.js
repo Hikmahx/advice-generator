3d360803cdc40a490180aff3112b1c70d77ca0d2
Hikmahx <hikmayousuph@gmail.com>
2022-04-21 16:06:43 -1100

    code refactor from usestate to usereducer hook

--- src/context/AdviceContext.js
+++ src/context/AdviceContext.js
@@ -1,38 +1,56 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import adviceReducer from "./AdviceReducer";
 
 const AdviceContext = createContext();
 
 export const AdviceProvider = ({ children }) => {
   useEffect(() => {
     getAdvice();
+    // eslint-disable-next-line
   }, []);
 
-  const [advice, setAdvice] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [errMsg, setErrMsg] = useState("");
-  const [error, setError] = useState(false);
+
+  const initialState = {
+    advice: {},
+    loading:false,
+    errMsg: '',
+    error: false
+  }
+
+  const [state, dispatch] = useReducer(adviceReducer, initialState)
 
 
   const getAdvice = async () => {
     try {
-      setLoading(true);
+      setLoading()
 
-      const response = await fetch("https://api.adviceslip.com/advice");
+      const response = await fetch("https://api.adviceslip.com/advice/71");
       const data = await response.json();
-      setAdvice(data.slip);
 
-      setLoading(false);
+      dispatch({
+        type: "GET_ADVICE",
+        payload: data.slip
+      })
     } catch (err) {
-      setError(true);
-      setErrMsg(err.message);
+      dispatch({
+        type: "SET_ERROR",
+        payload: err.message
+      })
     }
   };
+
+  // Set loading
+  const setLoading = ()=>
+  dispatch({
+    type: "SET_LOADING"
+  })
+
   return (
     <AdviceContext.Provider value={{
-      advice,
-      loading,
-      errMsg,
-      error,
+      advice: state.advice,
+      loading: state.loading,
+      errMsg: state.errMsg,
+      error: state.error,
       getAdvice
     }}>
       {children}
