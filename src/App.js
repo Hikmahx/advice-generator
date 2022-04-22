981beb092457deedd452500de8723a49a073fe46
Hikmahx <hikmayousuph@gmail.com>
2022-04-21 16:05:14 -1100

    attribution added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AdviceCard from "./components/AdviceCard";
+import Attribution from "./components/Attribution";
 import { AdviceProvider } from "./context/AdviceContext"; 
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <AdviceProvider>
       <div className="App font-Manrope bg-dark-blue h-screen flex items-start md:items-center justify-center pt-28 md:pt-0">
         <AdviceCard/>
+        <Attribution />
       </div>
     </AdviceProvider>
   );
---
+++ src/components/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Attribution = () => {
+  return (
+    <div className="attribution absolute pb-6 text-xs text-light-cyan mx-auto flex justify-center w-full bottom-4" role="contentinfo">
+      <div>Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" className='text-grayish-blue hover:text-neon-green'> Frontend Mentor</a></div>
+      <div>. Coded by <a href="https://github.com/Hikmahx" className='text-grayish-blue hover:text-neon-green'> Hikmah</a>.</div>
+    </div>
+  )
+}
+
+export default Attribution
