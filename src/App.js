3be51c7d3943a75dd3e999bfc25478935fa46b30
Hikmahx <hikmayousuph@gmail.com>
2022-04-21 16:59:37 -1100

    App.js updated

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { AdviceProvider } from "./context/AdviceContext";
 export default function App() {
   return (
     <AdviceProvider>
+      <>
       <main className="App font-Manrope bg-dark-blue h-screen flex items-start md:items-center justify-center pt-28 md:pt-0">
         <h1 className="absolute w-1 h-1 overflow-hidden">Advice Generator</h1>
         <AdviceCard/>
-        <Attribution />
       </main>
+      <Attribution />
+      </>
     </AdviceProvider>
   );
 }
